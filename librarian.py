f4af923fbb7d301c3cd762ae0d3981ed2faac886
Iago Mosquera <flackon@gmail.com>
2015-10-10 01:48:50 +0200

    finalizada modularización

--- librarian.py
+++ librarian.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 #
 #  __     ______     ______     __  __     ______
@@ -22,45 +22,33 @@
 
 import os
 import subprocess
+import yaml
 import logging
-from datetime import datetime, date, time
-#from subprocess import call
+import logging.handlers
 
 __author__ = 'Iago Mosquera'
 
-syncfile = "sync.txt"
-exclfile = "excl.txt"
-logfile = "/cygdrive/e/data/logs/librarian.log"
-logpath = "/cygdrive/e/data/logs/"
-synclog = "/cygdrive/e/data/logs/rsync.log"
+FILE_SETTINGS = "librarian_config.yml"
+FILE_LOG = "librarian.log"
+ENCODING = "utf-8"
 
-dir_descargas = "/cygdrive/e/Downloads"  # directorio de descargas
-baul = "/cygdrive/e/Downloads/baul"      # directorio donde guardar temporalmente archivos viejos
-trastero = "/cygdrive/e/trastero"        # directorio de almacenamiento de archivos viejos
-origin = "/cygdrive/e/"
-destination = "/cygdrive/f/backup2"
-dir_packages = "/cygdrive/e/archived/Packages/"
+settings = {}
+log = ""
 
-# Directorios protegidos que no deben moverse
-white_dirs = [
-    "video",
-    "img",
-    "audio",
-    "archff",
-    "binaries",
-    "data",
-    "clasificar",
-    "baul",
-    "torrent",
-    "jdl"
-]
 
-#
-# Funciones
-#
+def loadData(path):
+    return yaml.safe_load(open(path, 'r', encoding='utf-8'))
+
+
+def moverArchivos(lista, destino):
+    if len(lista) >= 1:
+        for line in lista:
+            subprocess.call(['mv', '-n', line, destino])
+            log.info("Moviendo archivo: {0}".format(line))
 
 
 def clasificarPorRegex(regex):
+    trastero = settings['dir_boxroom']
     result = subprocess.check_output([
         'find',
         trastero,
@@ -70,14 +58,9 @@ def clasificarPorRegex(regex):
         '-iregex', regex
         ])
     
-    return result.split('\n')
+    # print (result.split('\n'))
 
- 
-def moverArchivos(lista, destino):
-    if len(lista) > 1:
-        for line in lista:
-            subprocess.call(['mv', '-n', line, destino])
-            print "moviendo", line
+    return result.decode(ENCODING).split('\n')
 
 
 def comprimirBackup(nombre_arch, destino="/cygdrive/e/archived/Packages/", *entradas):
@@ -90,203 +73,183 @@ def comprimirBackup(nombre_arch, destino="/cygdrive/e/archived/Packages/", *entr
     os.system(cmd)
 
 
-def logline():
-    with open(logfile, "a") as logfd:
-        line = datetime.now().strftime("[%Y-%m-%d] @ %I:%M:%S %p - Librarian executed\n")
-        logfd.write(line)
+def getBoxPath(boxname):
+    trastero = settings['dir_boxroom']
 
-#
-# Setup
-#
-
-# Load preferences
-
-# Logging
-if not (os.path.exists(logpath)):
-    os.makedirs(logpath)
-log = logging.getLogger('librarian')
-formatter = logging.Formatter(fmt='{asctime} [{levelname}] - {message}', style='{')
-fileHandler = logging.handlers.RotatingFileHandler(os.path.join(logpath, "librarian.log"),
-    'a', 1048576, 10)
-fileHandler.setFormatter(formatter)
-log.setLevel(logging.INFO)
-log.addHandler(fileHandler)
-
-t_video = trastero + "/video"
-t_img = trastero + "/img"
-t_audio = trastero + "/audio"
-t_archive = trastero + "/archff"
-t_bin = trastero + "/binaries"
-t_data = trastero + "/data"
-t_mess = trastero + "/clasificar"
-
-dirs = [t_video, t_img, t_audio, t_archive, t_bin, t_data, t_mess]
-
-for dirname in dirs:
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-        #print "creando", dirname
-        log.info("creando directorio: {}", dirname)
+    return os.path.join(trastero, boxname)
 
 
-#
-# Copia de seguridad
-#
+def setupLogger(logFileName):
+    dir_logs = settings['dir_logs']
 
-# Ejecutar comando de copia de seguridad
-#rsync -avrh --delete-after --files-from=$LISTASYNC --log-file=$LOGFILE $OGDIR/d $BKDIR
+    if not (os.path.exists(dir_logs)):
+        os.makedirs(dir_logs)
 
-#
-# Limpieza de archivos
-#
+    log = logging.getLogger('librarian')
+    formatter = logging.Formatter(fmt='{asctime} [{levelname}] - {message}', style='{')
+    fileHandler = logging.handlers.RotatingFileHandler(os.path.join(dir_logs, logFileName),
+                                                       'a', 1048576, 10)
+
+    fileHandler.setFormatter(formatter)
+    log.setLevel(logging.INFO)
+    log.addHandler(fileHandler)
+
+    return log
 
 
-# Si el baúl no está vacío, mover al trastero
-if os.listdir(baul):
-    #result = subprocess.check_output(['ls', '-1', baul])
-    result = subprocess.check_output(['find', baul, '-maxdepth', '1'])
-    mover = result.split('\n')
-    if baul in mover:
-        mover.remove(baul)
-    #for f in mover: print(f)
-    moverArchivos(mover, trastero)
-    #subprocess.call(['mv', baul + '/*', trastero])
-else:
-    print("Baul vacío. Nada que mover")
-    
-# Mover descargas antiguas
-# Mover archivos
-result = subprocess.check_output([
-                'find',
-                dir_descargas,
-                '-maxdepth', '1',
-                '-type', 'f',
-                '-mtime', '+15',
-                ])
-#print(result)
-if result:
-    mover = result.split('\n')
-    for line in mover:
-        subprocess.call(['mv', line, baul])
-        print "moviendo", line
-        
-# Mover directorios
-result = subprocess.check_output([
-                'find',
-                dir_descargas,
-                '-maxdepth', '1',
-                '-type', 'd',
-                '-mtime', '+15',
-                ])
+def createBoxroomDirs():
+    dir_boxroom = settings['dir_boxroom']
 
-if result:
-    mover = result.split('\n')
-    if "" in mover:
-        mover.remove("")
-    if dir_descargas in mover:
-        mover.remove(dir_descargas)
-        #print("eliminado descargas")
-    for d in white_dirs:
-        #print(dir_descargas + "/" + d)
-        if dir_descargas + "/" + d in mover:
-            mover.remove(dir_descargas + "/" + d)
-            #print("eliminado " + d)
-    for line in mover:
-        subprocess.call(['mv', line, baul])
-        print "moviendo", line
+    for box in settings['boxroom']:
+        path = os.path.join(dir_boxroom, box['name'])
+        if not os.path.exists(path):
+            os.makedirs(path)
+            log.info("Creando directorio: {0}".format(path))
 
-# Clasificar trastero
-lista_archivos = clasificarPorRegex(".*\.(avi|mpg|mp4|wmv|mkv|flv|webm)")
-moverArchivos(lista_archivos, t_video)
 
-lista_archivos = clasificarPorRegex(".*\.(jpg|jpeg|png|gif)")
-moverArchivos(lista_archivos, t_img)
+def moveVaultToBoxroom():
+    vault = settings['dir_vault']
+    trastero = settings['dir_boxroom']
+    white_dirs = settings['white_dirs']
+    dir_descargas = settings['dir_dls']
 
-lista_archivos = clasificarPorRegex(".*\.(mp3|m4a|flac|ogg)")
-moverArchivos(lista_archivos, t_audio)
+    dias_antig = "+" + str(settings['days_old'])
 
-lista_archivos = clasificarPorRegex(".*\.(zip|rar|7z|gz|bz2|iso)")
-moverArchivos(lista_archivos, t_archive)
+    if os.listdir(vault):
+        result = subprocess.check_output(['find', vault, '-maxdepth', '1'])
+        mover = result.decode(ENCODING).split('\n')
+        if "" in mover:
+            mover.remove("")
+        if vault in mover:
+            mover.remove(vault)
+        moverArchivos(mover, trastero)
+    else:
+        log.info("Baul vacío. Nada que mover")
 
-lista_archivos = clasificarPorRegex(".*\.(exe|msi|msu|deb|rpm)")
-moverArchivos(lista_archivos, t_bin)
+    # Mover descargas antiguas
+    result = subprocess.check_output([
+                    'find',
+                    dir_descargas,
+                    '-maxdepth', '1',
+                    '-type', 'f',
+                    '-mtime', dias_antig,
+                    ])
 
-lista_archivos = clasificarPorRegex(".*\.(pdf|txt|doc|docx)")
-moverArchivos(lista_archivos, t_data)
+    if result:
+        mover = result.decode(ENCODING).split('\n')
+        if "" in mover:
+            mover.remove("")
+        # for line in mover:
+        #     subprocess.call(['mv', line, vault])
+        #     print "moviendo", line
+        moverArchivos(mover, vault)
+            
+    # Mover directorios
+    result = subprocess.check_output([
+                    'find',
+                    dir_descargas,
+                    '-maxdepth', '1',
+                    '-type', 'd',
+                    '-mtime', dias_antig,
+                    ])
 
-# Archivar directorios
-result = subprocess.check_output([
-                'find',
-                trastero,
-                '-maxdepth', '1',
-                '-type', 'd',
-                ])
+    if result:
+        mover = result.decode(ENCODING).split('\n')
+        if "" in mover:
+            mover.remove("")
+        if dir_descargas in mover:
+            mover.remove(dir_descargas)
+        for d in white_dirs:
+            # if dir_descargas + "/" + d in mover:
+            if os.path.join(dir_descargas, d) in mover:
+                mover.remove(os.path.join(dir_descargas, d))
+        moverArchivos(mover, vault)
 
-if result:
-    mover = result.split('\n')
-    if "" in mover:
-        mover.remove("")
-    if trastero in mover:
-        mover.remove(trastero)
-    for d in white_dirs:
-        #print(dir_descargas + "/" + d)
-        if trastero + "/" + d in mover:
-            mover.remove(trastero + "/" + d)
-            #print("eliminado " + d)
-    for line in mover:
-        subprocess.call(['mv', line, t_mess])
-        print "moviendo", line
 
-#
-# Borrar archivos temporales
-#
+def sortBoxroom():
+    boxroom = settings['boxroom']
+    trastero = settings['dir_boxroom']
+    white_dirs = settings['white_dirs']
 
-dir_tmp = "/cygdrive/e/tmp/*"
+    for set in boxroom:
+        # if len(set['filetypes']) > 0:
+        box = getBoxPath(set['name'])
+        regex = ".*\.("
+        for ext in set['filetypes'][:-1]:
+            regex = regex + ext + "|"
+        regex = regex + set['filetypes'][-1] + ")"
+        log.debug("Using regex: {0}".format(regex))
+        fileList = clasificarPorRegex(regex)
+        if fileList:
+            if "" in fileList:
+                fileList.remove("")
+            moverArchivos(fileList, box)
 
-print("Eliminando archivos temporales...")
-subprocess.call(['rm', '-rf', dir_tmp])
+    box = getBoxPath(settings['box_misc'])
+    result = subprocess.check_output([
+        'find',
+        trastero,
+        '-maxdepth', '1',
+        '-type', 'f',
+        ])
+    if result:
+        mover = result.decode(ENCODING).split('\n')
+        if "" in mover:
+            mover.remove("")
+        moverArchivos(mover, box)
 
-#
-# Crear archivos comprimidos
-#
+    result = subprocess.check_output([
+        'find',
+        trastero,
+        '-maxdepth', '1',
+        '-type', 'd',
+        ])
 
-print("Comprimiendo archivos extra...")
+    if result:
+        mover = result.decode(ENCODING).split('\n')
+        if "" in mover:
+            mover.remove("")
+        if trastero in mover:
+            mover.remove(trastero)
+        for d in white_dirs:
+            # if dir_descargas + "/" + d in mover:
+            if os.path.join(trastero, d) in mover:
+                mover.remove(os.path.join(trastero, d))
+        # for line in mover:
+        #     subprocess.call(['mv', line, t_mess])
+        #     print "moviendo", line
+        moverArchivos(mover, box)
 
-comprimirBackup('st3_pack-master', dir_packages, "/cygdrive/c/Users/Iago\ Mosquera/AppData/Roaming/Sublime\ Text\ 3/Packages/")
 
-# Self-backup
-comprimirBackup('librarian', dir_packages,
-                "/cygdrive/e/data/src/scripts/librarian/librarian.py",
-                "/cygdrive/e/data/src/scripts/librarian/settings_librarian.yml",
-                "/home/Iago\ Mosquera/sync.txt",
-                "/home/Iago\ Mosquera/excl.txt")
+def deleteTmp():
+    dir_tmp = settings['dir_tmp']
+    subprocess.call(['rm', '-rf', dir_tmp])
 
-#
-# Sincronizar copia de seguridad
-#
 
-# print("Sincronizando copia de seguridad...")
+def makeArchives():
+    dir_packages = settings['dir_packages']
 
-# subprocess.call([
-#     'rsync',
-#     '-avprh',
-#     '--delete-after',
-#     '--modify-window=3',
-#     '--files-from=' + syncfile,
-#     '--exclude-from=' + exclfile,
-#     '--log-file=' + synclog,
-#     origin,
-#     destination
-#     ])
+    for set in settings['archives']:
+        # using * to unpack the list into args
+        comprimirBackup(set['name'], dir_packages, *set['paths'])
 
-#
-# Escribir en el log
-#
 
-logline()
+def main():
+    global settings
+    global log
 
-#
-# Fin
-#
+    settings = loadData(FILE_SETTINGS)
+    log = setupLogger(FILE_LOG)
 
-print("Copia de seguridad terminada.")
+    createBoxroomDirs()
+    moveVaultToBoxroom()
+    # Backup would go here <-
+    sortBoxroom()
+    deleteTmp()
+    makeArchives()
+
+    log.info("Librarian - ejecución finalizada")
+
+
+if __name__ == '__main__':
+    main()
